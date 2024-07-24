04e68ddae67f3b3715b1a504c9fdcb2a024da5dc
dmora874 <derekmora874@gmail.com>
2024-07-24 11:01:35 -0600

    Working functionality. Starting UI.

--- WindmillWaterV1/Views/Routes/EditRouteView.swift
+++ WindmillWaterV1/Views/Routes/EditRouteView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import CoreData
 
 struct EditRouteView: View {
     @Environment(\.managedObjectContext) private var viewContext
@@ -6,23 +7,51 @@ struct EditRouteView: View {
     @ObservedObject var route: Route
     @State private var identifier: String
     @State private var date: Date
+    @State private var selectedCustomers: Set<Customer> = []
+
+    @FetchRequest(
+        entity: Customer.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \Customer.name, ascending: true)]
+    ) var customers: FetchedResults<Customer>
 
     init(route: Route) {
         self.route = route
         _identifier = State(initialValue: route.identifier ?? "")
         _date = State(initialValue: route.date ?? Date())
+        if let customers = route.customers as? Set<Customer> {
+            _selectedCustomers = State(initialValue: customers)
+        }
     }
 
     var body: some View {
-        Form {
-            Section(header: Text("Route Details")) {
-                TextField("Identifier", text: $identifier)
-                DatePicker("Date", selection: $date, displayedComponents: .date)
+        VStack {
+            Form {
+                Section(header: Text("Route Details")) {
+                    TextField("Identifier", text: $identifier)
+                    DatePicker("Date", selection: $date, displayedComponents: .date)
+                }
+            }
+
+            Form {
+                Section(header: Text("Customers")) {
+                    List {
+                        ForEach(customers, id: \.self) { customer in
+                            CustomerSelectionRow(customer: customer, isSelected: selectedCustomers.contains(customer)) {
+                                if selectedCustomers.contains(customer) {
+                                    selectedCustomers.remove(customer)
+                                } else {
+                                    selectedCustomers.insert(customer)
+                                }
+                            }
+                        }
+                    }
+                }
             }
 
             Button("Save") {
                 route.identifier = identifier
                 route.date = date
+                route.customers = NSSet(set: selectedCustomers)
                 saveContext()
                 presentationMode.wrappedValue.dismiss()
             }
@@ -38,5 +67,11 @@ struct EditRouteView: View {
             let nsError = error as NSError
             fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
+    }
+}
+
+struct EditRouteView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditRouteView(route: Route()).environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
